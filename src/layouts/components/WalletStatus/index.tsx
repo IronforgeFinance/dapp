165b6dc5ea36599a8e808160358855aaf485f5a0
savaris <18107744157@163.com>
2021-08-04 15:35:10 +0800

    update ui for home page

--- src/layouts/components/WalletStatus/index.tsx
+++ src/layouts/components/WalletStatus/index.tsx
@@ -13,84 +13,90 @@ import Clipboard from 'clipboard';
 import './index.less';
 
 export default () => {
-  const intl = useIntl();
-  const { account } = useWeb3React();
-  const { login, logout } = useAuth();
-  const [showDropdown, setShowDropdown] = useState(false);
+    const intl = useIntl();
+    const { account } = useWeb3React();
+    const { login, logout } = useAuth();
+    const [showDropdown, setShowDropdown] = useState(false);
 
-  const handleConnect = () => {
-    const connectorId = ConnectorNames.Injected;
-    login(connectorId);
-  };
+    const handleConnect = () => {
+        const connectorId = ConnectorNames.Injected;
+        login(connectorId);
+    };
 
-  const handleLogout = () => {
-    logout();
-  };
+    const handleLogout = () => {
+        logout();
+    };
 
-  const handleCopyAddress = () => {
-    const clipboard = new Clipboard('#copy-address');
-  };
+    const handleCopyAddress = () => {
+        const clipboard = new Clipboard('#copy-address');
+    };
 
-  const addressDisplay = useMemo(() => {
-    if (account) {
-      return account.substr(0, 5) + '...' + account.substr(-5);
-    }
-  }, [account]);
+    const addressDisplay = useMemo(() => {
+        if (account) {
+            return account.substr(0, 5) + '...' + account.substr(-5);
+        }
+    }, [account]);
 
-  const handleLink = () => {
-    const link = process.env.BSC_SCAN_URL + account;
-    window.open(link, '_blank');
-  };
+    const handleLink = () => {
+        const link = process.env.BSC_SCAN_URL + account;
+        window.open(link, '_blank');
+    };
 
-  const dropdownItem = (
-    <div className="dropdown">
-      <p className="item header">
-        <img src={IconMetamask} alt="" />
-        <button className="btn-common">Change</button>
-      </p>
-      <p className="item">{addressDisplay}</p>
-      <p className="item options">
-        <img
-          src={IconCopy}
-          alt=""
-          id="copy-address"
-          data-clipboard-text={account}
-          onClick={handleCopyAddress}
-        />
+    const dropdownItem = (
+        <div className="dropdown">
+            <p className="item header">
+                <img src={IconMetamask} alt="" />
+                <button className="btn-change btn-common">Change</button>
+            </p>
+            <p className="item">{addressDisplay}</p>
+            <p className="item options">
+                <img
+                    src={IconCopy}
+                    alt=""
+                    id="copy-address"
+                    data-clipboard-text={account}
+                    onClick={handleCopyAddress}
+                />
 
-        <img src={IconLink} alt="" onClick={handleLink} />
-      </p>
-      <p>
-        <button className="btn-disconnect" onClick={handleLogout}>
-          DISCONNECT
-        </button>
-      </p>
-    </div>
-  );
+                <img src={IconLink} alt="" onClick={handleLink} />
+            </p>
+            <p>
+                <button
+                    className="btn-disconnect common-btn-red"
+                    onClick={handleLogout}
+                >
+                    DISCONNECT
+                </button>
+            </p>
+        </div>
+    );
 
-  return (
-    <div className="wallet-status-container">
-      {!account && (
-        <button onClick={handleConnect} className="btn-common btn-connect">
-          {intl.formatMessage({ id: 'nav.connectWallet' })}
-        </button>
-      )}
-      {account && (
-        <Dropdown
-          overlay={dropdownItem}
-          trigger={['click']}
-          overlayClassName="custom-dropdown"
-          placement={'bottomLeft'}
-          visible={showDropdown}
-          onVisibleChange={(v) => setShowDropdown(v)}
-        >
-          <div className="wallet-connected">
-            <img src={IconBinance} alt="" />
-            <span> {addressDisplay}</span>
-            <img src={IconDown} alt="" />
-          </div>
-        </Dropdown>
-      )}
-    </div>
-  );
+    return (
+        <div className="wallet-status-container">
+            {!account && (
+                <button
+                    onClick={handleConnect}
+                    className="btn-common btn-connect"
+                >
+                    {intl.formatMessage({ id: 'nav.connectWallet' })}
+                </button>
+            )}
+            {account && (
+                <Dropdown
+                    overlay={dropdownItem}
+                    trigger={['click']}
+                    overlayClassName="custom-dropdown"
+                    placement={'bottomLeft'}
+                    visible={showDropdown}
+                    onVisibleChange={(v) => setShowDropdown(v)}
+                >
+                    <div className="wallet-connected">
+                        <img src={IconBinance} alt="" />
+                        <span> {addressDisplay}</span>
+                        <img src={IconDown} alt="" />
+                    </div>
+                </Dropdown>
+            )}
+        </div>
+    );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,27 @@ import './index.less';
 import useEagerConnect from '@/hooks/useEagerConnect';
 
 export default () => {
-  useEagerConnect();
-  return <div></div>;
+    useEagerConnect();
+    return (
+        <div className="home-container">
+            <div className="sheepskin-book">
+                <h3>Mint</h3>
+                <p className="key">Mint fUSD by staking a Token</p>
+                <p className="words">
+                    Mint fUSD by staking your Token. Token stakers earn weekly
+                    staking rewards in exchange for managing their
+                    Collateralization Ratio and debt.{' '}
+                    <a className="danger">Learn more</a>
+                </p>
+            </div>
+            <div className="sheepskin-book">
+                <h3>Trade</h3>
+                <p className="key">Borrow other fAsset by using your fAsset</p>
+                <p className="words">
+                    Borrow other fAsset using your fAsset{' '}
+                    <a className="danger">Learn more</a>
+                </p>
+            </div>
+        </div>
+    );
 };
