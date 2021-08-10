6f7012435d7157930b22af340c443b74e5a27630
savaris <18107744157@163.com>
2021-08-10 18:25:46 +0800

    update tab btn

---
+++ src/components/TabGroup/index.tsx
@@ -0,0 +1,38 @@
+import classNames from 'classnames';
+import React, { useCallback, useState } from 'react';
+import './index.less';
+
+interface TabOption {
+    name: string;
+    key: string | number;
+}
+
+interface TabGroupProps {
+    items: TabOption[];
+    value: string | number;
+    onChange: Function;
+}
+
+export default (props: TabGroupProps) => {
+    const { items = [], value, onChange } = props;
+
+    const _changeTab = useCallback((key) => onChange(key), []);
+
+    return (
+        <div className="tab-group">
+            {items.map((item) => {
+                return (
+                    <button
+                        className={classNames({
+                            'tab-btn': true,
+                            active: value === item.key,
+                        })}
+                        onClick={_changeTab.bind(this, item.key)}
+                    >
+                        {item.name}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
