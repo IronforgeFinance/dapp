b86bfed8bc5665a20fa99999e9302d4ec019a9bc
savaris <18107744157@163.com>
2021-08-11 17:23:25 +0800

    add big board for wallet

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import './index.less';
+import { useState } from 'react';
+import BigBoard from './BigBoard';
+
+const tabItems = [
+    {
+        name: 'Holding',
+        key: 'holding',
+    },
+    {
+        name: 'Pool',
+        key: 'pool',
+    },
+    {
+        name: 'Farm',
+        key: 'farm',
+    },
+    {
+        name: 'History',
+        key: 'history',
+    },
+];
 
 export default () => {
-  return <div></div>;
+    const [tabKey, setTabKey] = useState(tabItems[0].key);
+
+    return (
+        <div className="wallet">
+            <BigBoard
+                tabItems={tabItems}
+                tabKey={tabKey}
+                onChange={(key) => setTabKey(key)}
+            />
+        </div>
+    );
 };
