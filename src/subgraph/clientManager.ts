7121dbdfd4102e0d4e5c6a98b5bee20a8a5f783d
savaris <18107744157@163.com>
2021-09-01 11:26:45 +0800

    normalize graph typename

--- src/subgraph/clientManager.ts
+++ src/subgraph/clientManager.ts
@@ -1,11 +1,29 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, DefaultOptions, InMemoryCache } from '@apollo/client';
+
+/**
+ * @property {DefaultOptions} defaultOptions
+ * @description disable cache
+ */
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
+};
 
 export const pancakeswapClient = new ApolloClient({
     uri: process.env.PACAKE_GRAPH_URL,
     cache: new InMemoryCache(),
+    defaultOptions,
 });
 
 export const ourClient = new ApolloClient({
     uri: process.env.OUR_GRAPH_URL,
     cache: new InMemoryCache(),
+    defaultOptions,
 });
