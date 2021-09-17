ea07b4c2fee893cbe65722ecaad4e524fe0de803
savaris <18107744157@163.com>
2021-09-17 16:58:57 +0800

    clear all of timeout in npc dialog when leaved

--- src/components/NpcDialog/index.tsx
+++ src/components/NpcDialog/index.tsx
@@ -7,6 +7,7 @@ import {
     useEffect,
     ReactNode,
     useMemo,
+    useRef,
 } from 'react';
 import MintNpcPng from '@/assets/images/npc-dialog-mint-person.png';
 import HomeNpcPng from '@/assets/images/npc-dialog-home-person.png';
@@ -33,6 +34,9 @@ const NpcDialog = (props: NpcDialog) => {
     const [words, setWords] = useState('');
     const [slowWords, setSlowWords] = useState('');
     const [pathname, setPathname] = useState('/mint');
+    const tmKeys = useRef([]);
+    const delayKey = useRef(null);
+    // const totalTm = useRef(0);
     /**
      * @function appearSlowly
      * @description 逐渐输出文字的效果
@@ -43,13 +47,19 @@ const NpcDialog = (props: NpcDialog) => {
         const wordsArray = words.split('');
         wordsArray.forEach((word, index) => {
             const newWords = words.slice(0, index + 1);
-            console.log('>> alloc words to array -> %s', word);
-            setTimeout(() => setSlowWords(newWords), index * 100);
+            // console.log('>> alloc words to array -> %s', word);
+            tmKeys.current.push(
+                setTimeout(() => setSlowWords(newWords), index * 100),
+            );
         });
-        // setTimeout(close, wordsArray.length * 100 + 10000);
+
+        // 5s后自动隐藏
+        delayKey.current = setTimeout(close, wordsArray.length * 100 + 5000);
     }, [words]);
 
     const close = useCallback(() => {
+        clearTimeout(delayKey.current);
+        clearTms();
         setVisable(false);
 
         /**@description 关闭后清空文字，以便下次触发 */
@@ -57,22 +67,32 @@ const NpcDialog = (props: NpcDialog) => {
         setSlowWords('');
     }, [words]);
     const open = useCallback(() => {
+        clearTimeout(delayKey.current);
+        clearTms();
         setVisable(true);
 
         // 弹出需要一些时间，延迟输出文字的时间
         appearSlowly();
     }, [words]);
 
+    const isHome = useMemo(() => pathname === '/', [pathname]);
+
+    const clearTms = useCallback(
+        () => tmKeys.current.forEach((key) => clearTimeout(key)),
+        [],
+    );
+
     /**@description 若关闭，清空文字；反之打开窗口 */
     useEffect(() => (words?.length > 0 ? open() : close()), [words]);
 
-    const isHome = useMemo(() => pathname === '/', [pathname]);
-
+    /**@description 根据不同的路由弹出不同的NPC */
     useEffect(() => {
         setPathname(location!.pathname);
-
         return history.listen((location) => setPathname(location.pathname));
     }, []);
+
+    /**@description 清除所有timeout */
+    useEffect(() => clearTms, []);
 
     return (
         <NpcDialogContext.Provider
