a9889a51fb9def0ae703796569f3309539a09d7e
savaris <myz.defi@protonmail.com>
2021-10-19 18:35:44 +0800

    fix ui bugs

--- src/components/PreloadImage/index.tsx
+++ src/components/PreloadImage/index.tsx
@@ -5,22 +5,8 @@ interface PreloadImagesProps {
     image: string;
 }
 
-const commonFiles = [
-    'common-box-bg.558d532d.png',
-    'common-btn-red.8266a4fa.png',
-    'mint-select-token-dialog.efa361ab.png',
-    'header-logo.d4e433a9.png',
-    'npc-dialog-mint-person.617f727c.png',
-    'icon-axe.0b91ea44.png',
-    'icon-lock.1a41515a.png',
-    'icon-coin.6b5dcb6a.png',
-    'icon-magic-bottle.56c0fd18.png',
-];
-
 export const generateImageList = (files: string[]) => {
-    return files
-        .concat(commonFiles)
-        .map((file) => `${location.origin}/static/${file}`);
+    return files.map((file) => `${location.origin}/static/${file}`);
 };
 
 const PreloadImages = (props: PreloadImagesProps) => {
